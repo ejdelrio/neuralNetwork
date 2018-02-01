97152d0075cd7d67268d8ec0f5e73867b271df80
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-01 13:28:10 -0800

    Created multiple decgradient decent algorithms logistic regression with a 1st degree polynomial sigmoid function linear wit a 1st degree polynomial sigmoid linear with an nth degree sigmoid logistic with an nth degree polynomial sigmoid note** plan to combine sigmoids into singular function

--- gradientDecent/gdTemplate.js
+++ gradientDecent/gdTemplate.js
@@ -1,19 +1,8 @@
 'use strict';
 
 const cost = require('./cost.js');
-const {linearHypoth, logHypoth} = require('./hypoth.js');
-const  {slopeIntercept, polynomial} = require('./sigmoid.js');
 
-const gradientDescent = module.exports = {};
-/*Characterizes a single step in a gradient gradientDescent
-defined by the equation:
-	θj := 	θj - a/m * cost(θ, x)
-where θj represents the current theta value of a vector of thetas and
-j is a number representing the index from 0 to θvector.length - 1
-x is a row of a 2 dimesional matrix.
-*/
-
-gradientDescent.gdTemplate = function(
+module.exports = function(
   theta,
   data,
   alpha,
