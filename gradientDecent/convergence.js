050fb6f8a4f441d8c00483e0386198f1cfdebe70
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-05 16:00:33 -0800

    Deleted everything :D

--- gradientDecent/convergence.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-//A convergence output generated when the algorithm was left to run
-//for a long while :D
-module.exports = [
-  -6308.72669271521,
-  111.03863511102347
-];
--- gradientDecent/testRun.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const learning = require('./learning.js');
-const thetaArray = require('./thetaData.js');
-const matrix = require('./sampleData.js');
-
-console.log(learning(thetaArray, matrix, .00000025));
