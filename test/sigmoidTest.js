8dd9f24e5ae8a7823c79988e4e59f5c018465bbf
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-07 19:17:56 -0800

    Wrote test for first degree sigmoid function :D

--- test/sigmoidTest.js
+++ test/sigmoidTest.js
@@ -1,10 +1,19 @@
 'use strict';
 
 const expect = require('chai').expect;
+const sigmoid = require('../gradientDecent/sigmoid.js');
+
+let testTheta = [2, 3, 4, 5];
+let testRow = [2, 3, 4, 5];
 
 
 describe('Initial test for sigmoid functions', () => {
-  it('Should run a basic test for now', () => {
-    expect(2 + 2).to.equal(4);
+  describe('Tests standard sigmoid function of theta and xVector to first degree', () => {
+    it('Should return a single whole value', () => {
+      let sig = sigmoid.normal;
+      let yHypoth = sig(testTheta, testRow);
+      expect(yHypoth).to.equal(40);
+
+    });
   });
 });
