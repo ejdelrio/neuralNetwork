a9e48e3ebbd30cf7bea0a717cc8df92394644d3a
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-02-07 19:05:47 -0800

    Just added a sample test to ensure mocha and chai were working properly

---
+++ test/sigmoidTest.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+
+describe('Initial test for sigmoid functions', () => {
+  it('Should run a basic test for now', () => {
+    expect(2 + 2).to.equal(4);
+  });
+});
